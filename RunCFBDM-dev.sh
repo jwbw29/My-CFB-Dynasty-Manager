f20a2eeed5b14fb303afb94492e1b7382c664c08
Justin Byrd <justinbyrd7@gmail.com>
2025-08-07 09:59:49 -0500

    Stash changes

--- RunCFBDM-dev.sh
+++ RunCFBDM-dev.sh
@@ -4,6 +4,6 @@
 cd "$(dirname "$0")"
 
 echo "Starting development server on port 3002..."
-npm run dev -- -p 3002
+npm run electron-dev -- -p 3002
 
 echo "When done, press Ctrl+C to stop the app."
