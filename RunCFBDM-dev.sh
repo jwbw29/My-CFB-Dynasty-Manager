6b7c127a69d41949ad64e0a3cba29fa1149945f5
Justin Byrd <justinbyrd7@gmail.com>
2025-08-07 09:53:08 -0500

    Stash changes

---
+++ RunCFBDM-dev.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# Change to the directory where the script is located
+cd "$(dirname "$0")"
+
+echo "Starting development server on port 3002..."
+npm run dev -- -p 3002
+
+echo "When done, press Ctrl+C to stop the app."
